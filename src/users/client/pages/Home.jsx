c351c2370dedfbaf77825da3c9beeaf52758abcb
Ntye <ntye.nina@gmail.com>
2024-04-04 18:36:24 +0200

    add

--- src/users/client/pages/Home.jsx
+++ src/users/client/pages/Home.jsx
@@ -1,19 +1,25 @@
 // import React from 'react'
 import "../styles/Home.css"
-import Logo from "../../../assets/greenSquiggle.svg"
-import Logo1 from "../../../assets/pinkSquiggle.svg"
+// import Logo from "../../../assets/greenSquiggle.svg"
+// import Logo1 from "../../../assets/pinkSquiggle.svg"
 import Navbar from "../../../users/client/components/Navbar"
 
 const Home = () => {
   return (
     <div>
-      <div className="home-banner">
-        <img src={Logo1} className="navbar-image" />
-      </div>   
       <Navbar/>
-      <div className="home-banner-container">
-        <img src={Logo} alt="" />
-      </div>   
+      <div className="Header">
+
+      </div>
+
+      <div className="Body">
+        
+      </div>
+
+      <div className="Footer">
+        
+      </div>
+      
     </div>
     
   )
