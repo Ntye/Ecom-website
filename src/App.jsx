4f99ffbfaa4de806ef43761d0ed23d172e5d7208
Ntye <ntye.nina@gmail.com>
2024-04-06 22:31:23 +0200

    add

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import Index from "./index/Index"
 import Login from "./index/Login"
 import Home from "./users/client/pages/Home"
+import Signin from "./index/Signin"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Index/>}/>
           <Route path="/login" element={<Login/>}/>
           <Route path="/home" element={<Home/>}/>
+          <Route path="/sign-in" element={<Signin/>}/>
         </Routes>
       </Router>
     </>
