e663813aa4d0966b775088712d7e19203237d1c5
Ntye <ntye.nina@gmail.com>
2024-05-12 18:15:44 +0100

    add

--- src/index/Signup.jsx
+++ src/index/Signup.jsx
@@ -5,74 +5,74 @@ import "./styles/Connexion.css"
 import { Link } from 'react-router-dom';
 import Image from "../assets/log.svg"
 import swal from 'sweetalert'
-// import axios from 'axios';
+import {useEffect, useState} from "react";
+
+import axios from 'axios';
 const Signup = () => {
   const sexe = 'Sexe'
   const ville = 'Ville'
 
-  const handleSubmit =  (e) => {
-      e.preventDefault();
-      let error = {
-        title: 'You should enter the following field(s)\n\n',
-        message: '',
-        num: 0
-      }
-      const data = {}
-      const form = new FormData(e.currentTarget)
-      form.forEach((value, key)=>data[key] = value)
-      console.log(data)
+  const [cities, setCities] = useState([]);
 
-      if (data.nom.length === 0) {
-        error.message = error.message + '- Nom\n'
-        error.num = error.num +1
-      } 
-      if (data.prenom.length === 0) {
-        error.message = error.message + '- Prenom\n'
-        error.num = error.num +1
-      } 
-      if (data.username.length === 0) {
-        error.message = error.message + '- Username\n'
-        error.num = error.num +1
-      } 
-      if (data.pwd.length === 0) {
-        error.message = error.message + '- Password\n'
-        error.num = error.num +1
-      } 
-      if (data.confpwd.length === 0) {
-        error.message = error.message + '- Confirmation Password\n'
-        error.num = error.num +1
-      } 
-      if (data.numTel.length === 0) {
-        error.message = error.message + '- Telephone Number\n'
-        error.num = error.num +1
-      } 
-      if (data.dateNaissance.length === 0) {
-        error.message = error.message + '- Date of Birth\n'
-        error.num = error.num +1
-      } 
-      if(data.sexe === 'Sexe'){
-        error.message = error.message + '- Sexe\n'
-        error.num = error.num +1
-      } 
-      if(data.ville === 'Ville'){
-        error.message = error.message + '- Ville\n'
-        error.num = error.num +1
-      } 
-      if(error.num > 0) swal(error.message);
-      
-      else {
-        if(data.confpwd.localcompare(data.pwd) === 0){
-          delete data.confpwd
-          console.log(data)
-          // const url = "http://localhost/enquired.php";
-          // axios.post(url, fData)
-          //   .then(response => alert(response.data))
-          //   .catch(error => alert(error));
-        }
-        else{
-          swal("les mots de passe ne correspondent pas, veuillez reessayer!");
-        }
+  useEffect(() => {
+    const fetchCities = async () => {
+      try {
+        const response = await axios.get('http://127.0.0.1:8000/api/cities');
+
+        const { data } = response;
+
+        // setCities(data);
+      } catch (error) {
+        console.error('Error fetching cities:', error);
       }
+    };
+
+    fetchCities();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let error = {
+      title: 'You should enter the following field(s)\n\n',
+      message: ''
+    }
+    const data = {}
+    const form = new FormData(e.currentTarget)
+
+    let allFieldsNotEmpty = true;
+
+    for (const pair of form.entries()) {
+      if (pair[1].trim() === '') {
+        error.message = error.message + '- ' +pair[0] +'\n'
+        allFieldsNotEmpty = false;
+      }
+    }
+
+    if (allFieldsNotEmpty) {
+      console.log("All fields are not empty.");
+      const form = new FormData(e.currentTarget);
+
+      form.set('nom', form.get('nom') + ' ' + form.get('prenom'));
+
+      if ( form.get('pwd') === form.get('confpwd')) {
+        form.delete('prenom');
+        form.delete('confpwd');
+
+        form.forEach((value, key)=>data[key] = value)
+        console.log(data)
+
+        const url = "http://127.0.0.1:8000/api/sign-in";
+        const response = await axios.post(url, data)
+          // .then(response => alert(response.data))
+          // .catch(error => alert(error));
+      }
+      else {
+        swal("les mots de passe ne correspondent pas, veuillez reessayer!");
+      }
+      console.log(data)
+    } else {
+      console.log("At least one field is empty.");
+    }
   }
   return (
     <div className='centered'>
@@ -82,7 +82,7 @@ const Signup = () => {
           <Link to="/connexion" className='link-deco'>ici</Link></div>
         <div className='entries-pic'>
 
-          <Form style={{ width: '400px' }} onSubmit={handleSubmit} method='POST'>
+          <Form style={{ width: '400px' }} onSubmit={handleSubmit}  method='POST'>
             <Row className="mb-3">
               <Form.Group as={Col} controlId="Name">
                 <Form.Control name="nom" type="text" placeholder="Nom"/>
@@ -97,7 +97,7 @@ const Signup = () => {
             <span className='sub-text text-bel' >Vous pouvez utiliser les lettres les chiffres et les symboles </span>
             <Form.Group className="user" controlId="username">
               <Form.Control 
-                name='username'
+                name='user'
                 type='text'
                 placeholder="Nom d'utilisateur" 
               />
@@ -114,13 +114,16 @@ const Signup = () => {
 
             <Row className="mb-3">
               <Form.Group as={Col} controlId="Numero">
-                <Form.Control name="numTel" type="text" placeholder="Numero"/>
+                <Form.Control name="mobile" type="text" placeholder="Numero"/>
               </Form.Group>
 
               <Form.Group as={Col} controlId="Ville">
                 <Form.Select name="ville" defaultValue={ville}>
-                  <option value={ville} type='text'>Ville</option>
-                  <option value='....' type='text'>...</option>
+                  <option value='' type='text'>Ville</option>
+                  {cities.map((item) => (
+                    <option key={item.idVille} value={item.idVille}>{item.libelle}</option>
+                  ))}
+
                 </Form.Select>
               </Form.Group>
             </Row>
@@ -128,14 +131,14 @@ const Signup = () => {
             <span className='sub-text text-bel' >Date de Naissance </span>
             <Row className="mb-3">
               <Form.Group as={Col} controlId="Date">
-                <Form.Control name="dateNaissance" type="date" placeholder="Date de naissance"/>
+                <Form.Control name="dateNaiss" type="date" placeholder="Date de naissance"/>
               </Form.Group>
 
               <Form.Group as={Col} controlId="Sexe">
                 <Form.Select name='sexe'>
-                  <option value={sexe}>Sexe</option>
-                  <option value ="M" type='text'> Homme </option>
-                  <option value ="F" type='text'>Femme</option>
+                  <option value="">Sexe</option>
+                  <option value ="0" type='text'> Homme </option>
+                  <option value ="1" type='text'>Femme</option>
                 </Form.Select>
               </Form.Group>
             </Row>
