965e1351fe1b90d48adade5dd9f9c2bec2b85626
Ntye <ntye.nina@gmail.com>
2024-05-13 16:16:18 +0100

    add

--- src/users/components/NavbarC.jsx
+++ src/users/components/NavbarC.jsx
@@ -22,48 +22,48 @@ function NavbarC({navItems}) {
     };
 
     return (
-        <>
-            <Navbar key={'lg'} expand={'lg'} className="bg-body-tertiary mb-3 ">
-                <Container fluid>
-                    <Navbar.Brand href="#"> <img src={Logo} className='logo' alt='/'/> </Navbar.Brand>
-                    <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${'lg'}`} />
-                    <Navbar.Offcanvas
-                        id={`offcanvasNavbar-expand-${'lg'}`}
-                        aria-labelledby={`offcanvasNavbarLabel-expand-${'lg'}`}
-                        placement="start"
-                        className="custom-offcanvas"
-                    >
-                        <Offcanvas.Header closeButton>
-                            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${'lg'}`}>
-                                <img src={Logo} className='logo' alt='/'/>
-                            </Offcanvas.Title>
-                        </Offcanvas.Header>
-                        <Offcanvas.Body className='custom-body'>
-                            <Nav variant="underline" className="justify-content-end flex-grow-1 pe-3">
-                                <Form className="custom-search">
-                                    <Form.Control
-                                      name="search"
-                                      type="search"
-                                      placeholder="Search"
-                                      className="search-input"
-                                      aria-label="Search"
-                                    />
-                                    <Button variant="outline-success" className='custom-searchbutton'><Fa.FaSearch/> </Button>
-                                </Form>
-                                {navItems.map((item, index) => (
-                                    <NavComponent
-                                        key={item.id}
-                                        name={item.name}
-                                        icon={item.icon}
-                                        link={item.link}
-                                    />
-                                ))}
-                            </Nav>
-                        </Offcanvas.Body>
-                    </Navbar.Offcanvas>
-                </Container>
-            </Navbar>
-        </>
+      <>
+        <Navbar key={'lg'} expand={'lg'} className="bg-body-tertiary mb-3 ">
+          <Container fluid>
+            <Navbar.Brand href="#"> <img src={Logo} className='logo' alt='/'/> </Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${'lg'}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${'lg'}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${'lg'}`}
+              placement="start"
+              className="custom-offcanvas"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${'lg'}`}>
+                    <img src={Logo} className='logo' alt='/'/>
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body className='custom-body'>
+                <Nav variant="underline" className="justify-content-end flex-grow-1 pe-3">
+                  <Form className="custom-search">
+                    <Form.Control
+                      name="search"
+                      type="search"
+                      placeholder="Search"
+                      className="search-input"
+                      aria-label="Search"
+                    />
+                    <Button variant="outline-success" className='custom-searchbutton'><Fa.FaSearch/> </Button>
+                  </Form>
+                  {navItems.map((item, index) => (
+                    <NavComponent
+                      key={item.id}
+                      name={item.name}
+                      icon={item.icon}
+                      link={item.link}
+                    />
+                  ))}
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      </>
     );
 }
 
