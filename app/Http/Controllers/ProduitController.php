82fead582c6723ce8f9e5fba31ff50bd7b601003
Ntye <ntye.nina@gmail.com>
2024-05-16 16:17:01 +0100

    add

--- app/Http/Controllers/ProduitController.php
+++ app/Http/Controllers/ProduitController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Categorie;
 use App\Models\Produit;
 use Illuminate\Http\Request;
 
@@ -69,5 +70,59 @@ class ProduitController extends Controller
 
         // Return a response indicating success
         return response()->json(['message' => 'produit inserted successfully'], 201);
+    }
+
+//    public function search(Request $request)
+//    {
+//        $nomPro = $request->input('nomPro');
+//
+//        // Validate the input
+//        $request->validate([
+//            'nomPro' => 'required|min:3',
+//        ]);
+//
+//        // Perform the search
+//        $products = Produit::where('nomPro', 'LIKE', "%{$nomPro}%")->get();
+//
+//        // Return the search results to the view
+//        return response()->json(['items' => $products], 201);
+//    }
+
+    public function search(Request $request)
+    {
+        $query = $request->input('query');
+        $categoryId = $request->input('category_id');
+        $minPrice = $request->input('min_price');
+        $maxPrice = $request->input('max_price');
+
+        // Validate the input
+        $request->validate([
+            'query' => 'nullable|string|min:3',
+            'category_id' => 'nullable|exists:categories,id',
+            'min_price' => 'nullable|numeric|min:0',
+            'max_price' => 'nullable|numeric|min:0',
+        ]);
+
+        // Perform the search
+        $items = Produit::query()
+            ->when($query, function ($queryBuilder, $query) {
+                return $queryBuilder->where('nomPro', 'LIKE', "%{$query}%");
+            })
+            ->when($categoryId, function ($queryBuilder, $categoryId) {
+                return $queryBuilder->where('idCategorie', $categoryId);
+            })
+            ->when($minPrice, function ($queryBuilder, $minPrice) {
+                return $queryBuilder->where('price', '>=', $minPrice);
+            })
+            ->when($maxPrice, function ($queryBuilder, $maxPrice) {
+                return $queryBuilder->where('price', '<=', $maxPrice);
+            })
+            ->get();
+
+        // Fetch all categories for the dropdown
+        $categories = Categorie::all();
+
+        // Return the search results
+        return response()->json(['items' => $items, 'categories' => $categories]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -7,17 +7,7 @@ use App\Http\Controllers\VilleController;
 use App\Http\Controllers\ProduitController;
 use App\Http\Controllers\ClientController;
 use \App\Http\Controllers\PhotoController;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+use \App\Http\Controllers\CategorieController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -30,10 +20,15 @@ Route::get('/cities', [VilleController::class, 'index']);
 Route::post('/sign-in', [ClientController::class, 'signin']);
 Route::post('/login', [ClientController::class, 'login']);
 
+
+//On Categorie Table
+Route::get('/categories', [CategorieController::class, 'items']);
+
 //On Product Table
 Route::post('/new-product', [ProduitController::class, 'store']);
 Route::get('/all-products', [ProduitController::class, 'index']);
 Route::get('/products/{codePro}', [ProduitController::class, 'show']);
+Route::get('/search', [ProduitController::class, 'search']);
 
 //On Photo Table
 Route::post('/upload', [PhotoController::class, 'store']);
